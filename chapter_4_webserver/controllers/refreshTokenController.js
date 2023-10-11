6c4cc1247e53e3cda664aa435839588f0218b965
Darkhan Zhursin <daratechn@gmail.com>
2023-10-11 18:51:03 +0600

    modified crud and controllers

--- chapter_4_webserver/controllers/refreshTokenController.js
+++ chapter_4_webserver/controllers/refreshTokenController.js
@@ -1,8 +1,4 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
-
+const User = require('../model/User');
 const jwt = require('jsonwebtoken');
 
 const handleRefreshToken = async (req, res) => {
@@ -10,7 +6,7 @@ const handleRefreshToken = async (req, res) => {
     if (!cookies?.jwt) return res.sendStatus(401);
     const refreshToken = cookies.jwt;
 
-    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    const foundUser = await User.findOne({ refreshToken }).exec();
     if (!foundUser) return res.sendStatus(403); // Forbidden
     // evaluate jwt
     jwt.verify(
